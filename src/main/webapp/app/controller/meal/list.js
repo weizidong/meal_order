5d23a8d6b6995877d13bd9a024c8346675584ed2
linhaobin8898 <864037833@qq.com>
2015-05-22 13:17:28 +0800

    mealList

--- src/main/webapp/app/controller/meal/list.js
+++ src/main/webapp/app/controller/meal/list.js
@@ -4,11 +4,17 @@ define([ 'app', 'services/api/meal/mealApi', 'css!style/meal/list' ], function(a
 
 	app.register.controller('controller.meal.list', [ '$scope', '$rootScope', 'mealApi', '$location', function($scope, $rootScope, mealApi, $location) {
 
-//		alert($location.search().openId);
+		// alert($location.search().openId);
 		mealApi.getAll({
-			success : function(data){
+			success : function(data) {
 				$scope.mealList = data;
 			}
 		});
+
+		$scope.selectMeal = function(meal) {
+			if (confirm(meal.name + ' ' + meal.price + '元，确定？')) {
+
+			}
+		};
 	} ]);
 });
