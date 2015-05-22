a37b0d3478ef44fad8373db3e916b3359b43c747
linhaobin8898 <864037833@qq.com>
2015-05-22 11:02:43 +0800

    meal service

--- src/main/java/com/hbin/mealorder/service/meal/MealService.java
+++ src/main/java/com/hbin/mealorder/service/meal/MealService.java
@@ -1,7 +1,14 @@
 package com.hbin.mealorder.service.meal;
 
+import java.util.List;
+
 import com.hbin.mealorder.model.dao.meal.MealDao;
+import com.hbin.mealorder.model.entity.meal.Meal;
 
 public class MealService {
 	private MealDao mealDao = new MealDao();
+
+	public List<Meal> getAll() {
+		return mealDao.getAll();
+	}
 }
--- src/main/webapp/app/services/api/meal/mealService.js
+++ src/main/webapp/app/services/api/meal/mealService.js
@@ -2,5 +2,11 @@ define([ 'app', 'services/api/common/ajaxService' ], function(app) {
 
 	app.register.service('mealService', [ 'ajaxService', function(ajaxService) {
 
+		/**
+		 * 获取所有
+		 */
+		this.getAll = function(){
+			ajaxService.postJson(opts);
+		}
 	} ]);
 });
