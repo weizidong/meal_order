481d8c700b590132062ddf3b361de864e652fd99
linhaobin8898 <864037833@qq.com>
2015-05-22 17:50:29 +0800

    ...

--- src/main/webapp/app/controller/account/index.js
+++ src/main/webapp/app/controller/account/index.js
@@ -6,6 +6,30 @@ define([ 'app', 'services/api/account/accountApi', 'services/api/order/orderApi'
 
 		var accountId = $location.search().accountId;
 
+		// 是否已经开始点餐
+		orderApi.getMealOrder({
+			success : function(mealOrder) {
+				$scope.mealOrder = mealOrder;
+
+				// 获取帐号的订单
+				getAccountOrdeItems();
+			}
+		});
+
+		// 获取帐号的订单
+		function getAccountOrdeItems() {
+			orderApi.getAccountOrdeItems({
+				data : {
+					accountId : accountId,
+					orderId : $scope.mealOrder?$scope.mealOrder.id:''
+				},
+				success : function(accountOrders) {
+					$scope.accountOrders = accountOrders;
+				}
+			});
+		}
+
+		// 点餐
 		$scope.orderMeal = function() {
 			$location.path('/meal/list').search({
 				accountId : accountId
